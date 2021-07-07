ba4597a2fcad3bf5ead075e273ebb4327252c6e9
Noa van <78596993+Noavanbunge@users.noreply.github.com>
2021-07-07 13:11:24 +0000

    De galgjes die getekend worden met desbetreffende aantal levens

--- main.py
+++ main.py
@@ -1,3 +1,92 @@
-print("galgje")
-naam = input("wat is je naam?")
-print(naam)
+import random
+def spel() : 
+
+    def drawgalgjes():
+    if turns == 9:
+        print("Jammer, nog 8 pogingen")
+        print("""  
+         |
+         |
+         |
+         |
+         |
+    _____|""")
+
+    if turns == 8:
+        print("Jammer, nog 7 pogingen te gaan")
+        print("""     ____
+         |
+         |
+         |
+         |
+         |
+    _____|""")
+
+    if turns == 7:
+        print("Jammer, nog 6 pogingen te gaan")
+        print("""     ____
+        \|
+         |
+         |
+         |
+         |
+    _____|""")
+
+    if turns == 6:
+        print("Jammer, nog 5 pogingen te gaan")
+        print("""     ____
+      | \|
+         |
+         |
+         |
+         |
+    _____|""")
+
+    if turns == 5:
+        print("Jammer, nog 4 pogingen te gaan")
+        print("""     ____
+      | \|
+      o  |
+         |
+         |
+         |
+    _____|""")
+
+    if turns == 4:
+        print("Jammer, nog 3 pogingen te gaan")
+        print("""     ____
+      | \|
+      o  |
+      |  |
+         |
+         |
+    _____|""")
+
+    if turns == 3:
+        print("Jammer, nog 2 poging te gaan")
+        print("""     ____
+      | \|
+      o  |
+     /|\ |
+         |
+         |
+    _____|""")
+
+    if turns == 2:
+        print("Pas op!!!, nog 1 poging te gaan")
+        print("""     ____
+      | \|
+      o  |
+     /|\ |
+     / \ |
+         |
+    _____|""")
+
+    if turns == 1:
+        print("""     ____
+      | \|
+      o  |
+     /|\ |
+     / \ |
+         |
+    _____|""")
---
+++ woordenlijst.py
@@ -0,0 +1,13 @@
+word_list = [
+  'cavia',
+  'hamster'
+  'walvis',
+  'hond',
+  'konijn',
+  'rendier',
+  'spin',
+  'olifant',
+  'tijger',
+  'leeuw',
+  'giraffe'
+]
